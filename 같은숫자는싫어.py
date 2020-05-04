7ffe844b56a376ba8e272f4a94b0c0ece5e8946d
bpeak <bpeakcpeak@gmail.com>
2020-05-04 13:44:02 +0900

    same number

---
+++ 같은숫자는싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+	result = []
+	result.append(arr[0])
+	for i in range(1, len(arr)):
+		if arr[i - 1] != arr[i]:
+			result.append(arr[i])
+	return result
+
+print(solution([4,4,4,3,3]))
