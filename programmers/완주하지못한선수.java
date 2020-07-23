795f3485b47fcd98a3c438b163f370ac40ab522c
bpeak <bpeakcpeak@gmail.com>
2020-07-23 21:31:02 +0900

    2020년 07월 23일 목 오후  9:31:02

---
+++ programmers/완주하지못한선수.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> hash = new HashMap<String, Integer>();
+        for(String p : completion) {
+            if(!hash.containsKey(p)) {
+                hash.put(p, 0);
+            }
+            hash.put(p, hash.get(p) + 1);
+        }
+        for(String p : participant) {
+            if(!hash.containsKey(p)) { 
+                return p;
+            }
+            hash.put(p, hash.get(p) - 1);
+            if(hash.get(p) < 0) {
+                return p;
+            }
+        }
+        return null;
+    }
+}
+
+class SolutionTwo {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> hash = new HashMap<>(); // 다이아몬드 연산자
+        String result = null;
+        for(String p : completion) {
+            hash.put(p, hash.getOrDefault(p, 0) + 1); // hashMap.getOrDefault(key, defaultValue);
+        }
+        for(String p : participant) {
+            if(!hash.containsKey(p)) {
+                result = p;
+                break;
+            }
+            hash.put(p, hash.get(p) - 1);
+            if(hash.get(p) < 0) {
+                result = p;
+                break;
+            }
+        }
+        return result;
+    }
+}
