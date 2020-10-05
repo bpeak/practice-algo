5b2da6296829055c7bbb2558eeae45f6fda84558
bpeak <bpeakcpeak@gmail.com>
2020-10-05 23:49:07 +0900

    2020년 10월  5일 월 오후 11:49:07

--- programmers/카펫.py
+++ programmers/카펫.py
@@ -11,3 +11,18 @@ def solution(brown_count, yellow_count):
         curr_yellow_count = (width - 2) * (height - 2)
         if yellow_count == curr_yellow_count:
             return [width, height]
+
+def solution2(brown_count, yellow_count):
+    total_count = brown_count + yellow_count
+    for height in range(3, total_count):
+        if total_count % height != 0: continue
+        width = total_count // height
+        curr_yellow_count = (width - 2) * (height - 2)
+        if curr_yellow_count == yellow_count:
+            return [width, height]
+
+print(solution(10, 2)) # 4 3
+print(solution(24, 24)) # 8 6
+
+print(solution2(10, 2)) # 4 3
+print(solution2(24, 24)) # 8 6
